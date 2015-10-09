f35215a279ee507063c87844a6a45da43d01b876
Brian Atkins <brian_tkins@yahoo.com>
2015-10-08 19:04:41 -0700

    My initial creation
    My initial creation of my portfolio website

---
+++ MyPortfolioWebsite.js
@@ -0,0 +1,20 @@
+document.getElementById("github").onclick = function () {
+
+    location.href = "https://github.com/BrianAtk/";
+};
+
+$("#weatherApp").click(function () {
+    alert("got here");
+    location.href ="http://codepen.io/BrianAtk/full/wKadBO/";
+});
+
+document.getElementById("freecodecamp").onclick = function () {
+    alert("got there");
+    location.href = "http://www.freecodecamp.com/brianatk";
+};
+
+
+
+!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p + '://platform.twitter.com/widgets.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'twitter-wjs');
+
+
